64bf2f8e66afb93cd53e9aeb8a923843cf1a5781
LeeSeungCheon <koogk7@gmail.com>
2019-08-31 17:57:02 +0900

    세대별 메시지 창 팝업 구현완료

--- home/js/houseInfo.js
+++ home/js/houseInfo.js
@@ -1,4 +1,5 @@
 let dummy = [];
+let address = "아파트";
 
 function makeDummy() {
     let i = 0;
@@ -12,6 +13,24 @@ function makeDummy() {
         i += 1;
     }
     return dummy;
+}
+
+function fetchHouseList() {
+    let house_url = "http://ec2-54-180-24-178.ap-northeast-2.compute.amazonaws.com:8080/api/home/" + address
+
+    fetch(house_url, {
+        method : "GET",
+        headers: {
+            Accept: 'application/json',
+        }
+    })
+        .then( response => response.json())
+        .then( response => {
+            console.log(response);
+            loadHouseList(response);
+            dummy = {response};
+        })
+
 }
 
 function loadHouseList(list=dummy) {
@@ -51,15 +70,33 @@ function createRoomBox(item) {
     room_number.classList.add("room-number");
 
     //색 변경 작업 및 클릭 이벤트 달기
-    room_number.textContent = item.home_no;
+    room_number.textContent = item.home_No;
     room_number_box.appendChild(room_number);
+    room_number_box.addEventListener('click', roomClickHandler);
+    room_number_box.setAttribute("name", item.message); // 안티패턴
+    room_number_box.setAttribute("data-toggle", "modal");
+    room_number_box.setAttribute("data-target", "#roomMsg");
 
     if(item.message !== ''){
-        let room_noti = document.createElement("div");
-        room_noti.classList.add("room-noti");
-        room_noti.innerText = "v";
-        room_number_box.appendChild(room_noti);
+        let img = document.createElement("img");
+        img.classList.add("room-noti");
+        img.setAttribute("src", "../img/msg_noti.png");
+        room_number_box.appendChild(img);
     }
+
     return room_number_box;
 }
 
+function roomClickHandler(e) {
+    let room_number = e.currentTarget.querySelector(".room-number").textContent;
+    let msg = e.currentTarget.getAttribute("name");
+    let modal_header = document.querySelector(".modal-header .modal-title");
+    let modal_body = document.querySelector(".modal-body");
+
+    modal_header.innerHTML = room_number + "호";
+    if(msg === "")
+        modal_body.innerHTML = "공고된 메시지가 없습니다.";
+    else
+        modal_body.innerHTML = msg;
+
+}
